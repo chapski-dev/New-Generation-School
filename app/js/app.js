fd57d66f6385d8d39360e88becb49ee79483a340
Valentin Lipai <valentine.webdev@gmail.com>
2021-08-07 18:50:21 +0300

    minor fix

--- app/js/app.js
+++ app/js/app.js
@@ -132,26 +132,11 @@ if (accordion.length) {
     parent.siblings().removeClass('active');
     parent.toggleClass('active');
   })
+
+  $('.accordion__show-all').on('click', 'a', function (event) {
+    event.preventDefault();
+    accordion.find('.accordion-item[style]').removeAttr('style');
+    $(this).remove();
+  });
 }
 
-// const accordionItemHeader = document.querySelectorAll(".accordion-item__header")
-
-// accordionItemHeader.forEach(accordionItemHeader => {
-//   accordionItemHeader.addEventListener("click", event => {
-//     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item__header.active");
-//     if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
-//       currentlyActiveAccordionItemHeader.classList.toggle("active");
-//       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//     }
-
-
-//     accordionItemHeader.classList.toggle("active");
-//     const accordionItemBody = accordionItemHeader.nextElementSibling;
-//     if (accordionItemHeader.classList.contains("active")) {
-//       accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-//     }
-//     else {
-//       accordionItemBody.style.maxHeight = 0;
-//     }
-//   })
-// });
