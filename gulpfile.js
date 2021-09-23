83523f60b056506e5bad8c2c85fa680d79ab79f6
Valentin Lipai <valentine.webdev@gmail.com>
2021-09-23 21:02:58 +0200

    added xml version to svg files

--- gulpfile.js
+++ gulpfile.js
@@ -22,29 +22,6 @@ const rsync        = require('gulp-rsync')
 const del          = require('del')
 const fileinclude  = require('gulp-file-include')
 
-function browsersync() {
-	browserSync.init({
-		server: {
-			baseDir: 'app/',
-			middleware: bssi({ baseDir: 'app/', ext: '.html' })
-		},
-		ghostMode: { clicks: false },
-		notify: false,
-		online: true,
-		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
-	})
-}
-
-  /** HTML */
-  function html() {
-	return src('app/pages/*.html')
-	  .pipe( fileinclude({
-		prefix: '@@',
-		basepath: 'app/html-components'
-	  }) )
-	  .pipe( dest('app') )
-  }
-
 function scripts() {
 	return src(['app/js/*.js', '!app/js/*.min.js'])
 		.pipe(webpack({
@@ -83,62 +60,11 @@ function styles() {
 		.pipe(browserSync.stream())
 }
 
-function images() {
-	return src(['app/images/src/**/*'])
-		.pipe(newer('app/images/dist'))
-		.pipe(imagemin())
-		.pipe(dest('app/images/dist'))
-		.pipe(browserSync.stream())
-}
-
-function buildcopy() {
-	return src([
-		'{app/js,app/css}/*.min.*',
-		'app/images/**/*.*',
-		'!app/images/src/**/*',
-		'app/fonts/**/*'
-	], { base: 'app/' })
-	.pipe(dest('dist'))
-}
-
-async function buildhtml() {
-	let includes = new ssi('app/', 'dist/', '/**/*.html')
-	includes.compile()
-	del('dist/parts', { force: true })
-}
-
-function cleandist() {
-	return del('dist/**/*', { force: true })
-}
-
-function deploy() {
-	return src('dist/')
-		.pipe(rsync({
-			root: 'dist/',
-			hostname: 'username@yousite.com',
-			destination: 'yousite/public_html/',
-			// clean: true, // Mirror copy with file deletion
-			include: [/* '*.htaccess' */], // Included files to deploy,
-			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
-			recursive: true,
-			archive: true,
-			silent: false,
-			compress: true
-		}))
-}
-
 function startwatch() {
 	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
 	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
-	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
-	watch(['app/pages/*.html', 'app/html-components/**/*.html'], html)
-	watch([`app/**/*.{${fileswatch}}`,'!app/pages/*.html', '!app/html-components/**/*.html'], { usePolling: true }).on('change', browserSync.reload)
 }
 
 exports.scripts = scripts
 exports.styles  = styles
-exports.images  = images
-exports.deploy  = deploy
-exports.assets  = series(scripts, styles, images)
-exports.build   = series(cleandist, scripts, styles, images, buildcopy, buildhtml)
-exports.default = series(html, scripts, styles, images, parallel(browsersync, startwatch))
+exports.default = series(scripts, styles, parallel(startwatch))
